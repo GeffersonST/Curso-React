8e05c1b147bdb3aee5f7bd9892293175a180081d
GeffersonST <geffsevero@gmail.com>
2021-09-09 23:09:00 -0300

    Feed curtidas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
+import Feed from "./components/Feed";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hora: "00:00:00",
+      feed: [
+        { id: 1, username: "Gefferson", curtidas: 100, comentarios: 3 },
+        { id: 2, username: "Gerson", curtidas: 90, comentarios: 80 },
+        { id: 3, username: "Emile", curtidas: 400, comentarios: 60 },
+        { id: 3, username: "Gabriela", curtidas: 90, comentarios: 20 },
+      ],
     };
   }
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({ hora: new Date().toLocaleTimeString() });
-    }, 1000);
-  }
-  componentDidUpdate() {
-    console.log("Atualizou!");
-  }
-
   render() {
     return (
       <div>
-        <h1>Meu Projeto {this.state.hora}</h1>
+        {this.state.feed.map((item) => {
+          return (
+            <Feed
+              id={item.id}
+              username={item.username}
+              curtidas={item.curtidas}
+              comentarios={item.comentarios}
+            />
+          );
+        })}
       </div>
     );
   }
 }
-
 export default App;
