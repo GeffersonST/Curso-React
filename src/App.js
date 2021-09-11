3b902ab9ccd246dc5a715d91a6908e47e3ee77cc
GeffersonST <geffsevero@gmail.com>
2021-09-11 12:17:49 -0300

    Formulário

--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
 import React, { Component } from "react";
-import Feed from "./components/Feed";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      feed: [
-        { id: 1, username: "Gefferson", curtidas: 100, comentarios: 3 },
-        { id: 2, username: "Gerson", curtidas: 90, comentarios: 80 },
-        { id: 3, username: "Emile", curtidas: 400, comentarios: 60 },
-        { id: 3, username: "Gabriela", curtidas: 90, comentarios: 20 },
-      ],
+      form: {
+        nome: "",
+        email: "",
+        senha: "",
+        sexo: "",
+      },
     };
+    this.dadosForm = this.dadosForm.bind(this);
   }
+  dadosForm(e) {
+    let form = this.state.form;
+    form[e.target.name] = e.target.value;
+    this.setState({ form: form });
+  }
+
   render() {
     return (
       <div>
-        {this.state.feed.map((item) => {
-          return (
-            <Feed
-              id={item.id}
-              username={item.username}
-              curtidas={item.curtidas}
-              comentarios={item.comentarios}
-            />
-          );
-        })}
+        <h2> Login</h2>
+        Nome:
+        <input
+          type="text"
+          name="nome"
+          value={this.state.form.nome}
+          onChange={this.dadosForm}
+        />{" "}
+        <br />
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={this.state.form.email}
+          onChange={this.dadosForm}
+        />{" "}
+        <br />
+        Senha:
+        <input
+          type="password"
+          name="senha"
+          value={this.state.form.senha}
+          onChange={this.dadosForm}
+        />{" "}
+        <br />
+        Sexo:{" "}
+        <select
+          name="sexo"
+          value={this.state.form.sexo}
+          onChange={this.dadosForm}
+        >
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+        </select>
+        <div>
+          <h3>{this.state.form.email}</h3>
+          <h3>{this.state.form.senha}</h3>
+          <h3>{this.state.form.sexo}</h3>
+        </div>
       </div>
     );
   }
